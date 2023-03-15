5b9ae2684a5cd206b92139ae2930440034a0d54d
hanyun <1355081829@qq.com>
2023-03-15 23:51:47 +0800

    增加fakerjs，生成测试数据

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ApiMapResponse } from './decorator/api.map.response';
 import { success } from './utils/response';
 import { CustomerService } from './customer/customer.service';
 import { AdminService } from './admin/admin.service';
 import * as bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
 
 @ApiTags('app')
 @Controller()
@@ -28,13 +28,13 @@ export class AppController {
   }
   async initAdmin() {
     const saltOrRounds = 10;
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 50; i++) {
       const password = '1355081829@qq.com';
       const hash = await bcrypt.hash(password, saltOrRounds);
       const admin = {
-        username: `customer ${i}`,
-        mobile: `157013088${i}`,
-        email: `13550818${i}@qq.com`,
+        username: faker.internet.userName(),
+        mobile: faker.phone.number(),
+        email: faker.internet.email(),
         password: hash,
       };
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -43,11 +43,11 @@ export class AppController {
     }
   }
   async initCustomer() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 50; i++) {
       const customer = {
-        username: `customer ${i}`,
-        mobile: `157013088${i}`,
-        email: `13550818${i}@qq.com`,
+        username: faker.internet.userName(),
+        mobile: faker.phone.number(),
+        email: faker.internet.email(),
       };
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
